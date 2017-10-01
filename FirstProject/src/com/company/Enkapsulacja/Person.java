e6ca431d99776b37634e6c4cb392ed6971ba1d23
GrzesiekXiaoyo <31144065+GrzesiekXiaoyo@users.noreply.github.com>
2017-10-01 23:54:15 +0200

    Enkapsulacja added

---
+++ FirstProject/src/com/company/Enkapsulacja/Person.java
@@ -0,0 +1,20 @@
+package com.company.Enkapsulacja;
+
+public class Person {
+
+    //chcemy zadbac o to zeby do Name nikt nie wpisal Nulla, dzieki metodzie mozemy zadbac o to co sie stanie zanim zostanie przypisane
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) throws Exception {
+        if(name==null||name.length()<4)
+        {
+            throw new Exception();
+        }
+
+        Name = name;
+    }
+}
