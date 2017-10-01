a3ef90a664479653866bbb34580dcd376cf506cd
GrzesiekXiaoyo <31144065+GrzesiekXiaoyo@users.noreply.github.com>
2017-10-01 23:28:01 +0200

    Polimorfizm

---
+++ FirstProject/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.Polimorfizm.PolimorfizmTester;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        PolimorfizmTester.TestPolimorfizm();
+
+    }
+}
---
+++ FirstProject/src/com/company/Polimorfizm/Dog.java
@@ -0,0 +1,9 @@
+package com.company.Polimorfizm;
+
+public class Dog implements IAnimal {
+
+    @Override
+    public void GiveVoice() {
+        System.out.println("Hau hau");
+    }
+}
