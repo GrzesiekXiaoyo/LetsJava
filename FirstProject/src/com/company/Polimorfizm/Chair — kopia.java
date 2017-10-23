d19363c721f4daa6f12024c0dd57250529655e7c
Xiaoyo <grzesiekk15@gmail.com>
2017-10-23 19:12:24 +0200

    commit

---
+++ FirstProject/src/com/company/Polimorfizm/Chair — kopia.java
@@ -0,0 +1,10 @@
+package com.company.Polimorfizm;
+
+public class Chair implements IGoForward {
+
+    @Override
+   public void GoForward()
+   {
+
+   }
+}
--- FirstProject/src/com/company/Polimorfizm/Dog — kopia.java
+++
@@ -1,9 +0,0 @@
-package com.company.Polimorfizm;
-
-public class Dog implements IAnimal,IGoForward {
-
-    @Override
-    public void GiveVoice() {
-        System.out.println("Hau hau");
-    }
-}
